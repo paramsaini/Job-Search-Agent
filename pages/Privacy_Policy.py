6a50d562212971201775745dfa1945fb1bd0c237
paramjeet singh saini <86552866+paramsaini@users.noreply.github.com>
2026-01-30 12:10:04 +0000

    Update Privacy_Policy.py

--- pages/Privacy_Policy.py
+++ pages/Privacy_Policy.py
@@ -85,7 +85,7 @@ We may update this Privacy Policy from time to time. We will notify you of any c
 
 If you have any questions about this Privacy Policy, please contact us at:
 
-**Email:** paramsaini0072008@gmail.com
+**Email:** support@job-search-agent.com
 
 ---
 
